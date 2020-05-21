541ef8c3c9a1c2874af1a38b664e0efbe836ebbd
axd18 <adrianfernandezj@hotmail.com>
2020-05-20 21:44:27 -0300

    115-validando-formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 
 function App() {
+  
+  // State del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: '' 
+   });
+  
+   const { ciudad, pais } = busqueda;
+
+   useEffect(() => {
+      console.log(ciudad);
+   },[ciudad, pais]);
+  
   return (
     <Fragment>
       <Header
@@ -13,7 +26,10 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+              />
             </div>
             <div className="col m6 s12">
               2
